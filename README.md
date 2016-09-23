b3eb5141da9975ffab02624b435867b1c766bf58
Tony <tlastowka@gmail.com>
2016-09-23 15:20:55 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# python-haikubot
+
+a python 3 slack haiku detetor for nate
+
+basic bot logic almost entirely from starterbot: https://www.fullstackpython.com/blog/build-first-slack-bot-python.html
+
+basic hakiu logic and the syllables file from toby:  http://www.metaltoad.com/blog/writing-haiku-detecting-bot-slack
+
+# howto
+
+1) Set BOT_ID and SLACK_BOT_TOKEN as environent variables.  The page for starterbot has good instructions if you dont know how
+2) run the bot
+3) the bot will watch channels it has access to.  if it finds a haiku, it will let the channel know
+
---
+++ haiku.py
@@ -0,0 +1,83 @@
+import os
+import time
+from slackclient import SlackClient
+import json
+
+BOT_ID = os.environ.get("BOT_ID")
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+
+with open('syllables.json') as fh:
+    SYLLABLES = json.load(fh)
+
+
+def handle_command(potential, channel):
+    """
+        parse the potential haiku
+        if it is a haiku, let the channel it came from know
+    """
+
+    print('handling {0}'.format(potential))
+
+    # split up the words and strip common punctuation
+    words = [w.rstrip('.').rstrip('?').rstrip('!') for w in
+        potential.strip().upper().split()]
+
+    count_syl = 0
+    for word in words:
+        try:
+            count_syl += SYLLABLES[word]
+        except KeyError:
+            print("""don't know the word {0}""".format(word))
+            return
+
+    if count_syl == 17:
+        response = """found a haiku! '{0}'""".format(potential)
+        print(response)
+        slack_client.api_call("chat.postMessage", channel=channel,
+                          text=response, as_user=True)
+    else:
+        print('{0} with {1} syllables is not a haiku'.format(
+            potential, count_syl))
+
+
+def parse_slack_output(slack_rtm_output):
+    """
+        grab slack output on channels we are in.
+        filter out non messages, and messages from ourself
+        send the rest to handle command
+    """
+
+    output_list = slack_rtm_output
+    if output_list and len(output_list) > 0:
+        for output in output_list:
+            if output:
+                if not output.get('type') == 'message':
+                    # ignore non messages
+                    continue
+                elif output.get('user') == BOT_ID:
+                    # ignore messages from self
+                    continue
+
+                potential = output.get('text') or ''
+                if not len(potential):
+                    continue
+                channel = output.get('channel')
+                print('channel {0}, potential {1}'.format(
+                    channel, potential))
+                return potential, channel
+                #if potential and channel:
+                #    return potential.split().strip(), channel
+    return None, None
+
+
+if __name__ == "__main__":
+    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
+    if slack_client.rtm_connect():
+        print("StarterBot connected and running!")
+        while True:
+            command, channel = parse_slack_output(slack_client.rtm_read())
+            if command and channel:
+                handle_command(command, channel)
+            time.sleep(READ_WEBSOCKET_DELAY)
+    else:
+        print("Connection failed. Invalid Slack token or bot ID?")
